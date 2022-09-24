c57283c22d26335a4310d6cfc54ef7353e848742
Divya-Gunda <M1074001@granicus.com>
2022-09-24 09:16:11 +0530

    uploading ECR repo code13

--- providers.tf.tf
+++ providers.tf.tf
@@ -8,23 +8,21 @@ terraform {
   }
 }
 
-terraform {
- required_providers {
-     aws = {
-        source = "hashicorp/aws"
-        version = "v1.3.0"
-    }
- }
-}
-
-provider "aws" {
-  shared_credentials_file = "/home/ubuntu/.aws/credentials"
-  //profile = "bttstaging"
-  region =  var.region
-}
-
 provider "docker" {
 
 }
+
+/*provider "aws" {
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  //profile = "bttstaging"
+  region =  var.region
+
+   aws = {
+        source = "hashicorp/aws"
+        version = "v1.3.0"
+    }
+}*/
+
+
 
 
