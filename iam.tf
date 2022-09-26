eef97d3c09927df92a9ebe60cdbfcb2916a7883b
Divya-Gunda <M1074001@granicus.com>
2022-09-26 09:58:38 +0530

    uploading ECR repo code48

--- iam.tf
+++ iam.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "ecs_assume_role" {
 
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = var.ecs_task_execution_role
+  name               = var.ecs_task_execution_role_name
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
 }
 
