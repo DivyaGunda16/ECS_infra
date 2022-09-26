dcf4ce250a7b45687aa427307a5817156f4f14c3
Divya-Gunda <M1074001@granicus.com>
2022-09-26 09:55:01 +0530

    uploading ECR repo code47

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 ## ECS task execution role data
-data "aws_iam_policy_document" "ecs_task_execution_role" {
+data "aws_iam_policy_document" "ecs_assume_role" {
   version = "2012-10-17"
   statement {
     sid     = ""
@@ -19,11 +19,11 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = var.ecs_task_execution_role
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
 }
 
 # ECS task execution role policy attachment
-resource "aws_iam_policy" "ecs_permissions" {
+/*resource "aws_iam_policy" "ecs_permissions" {
   name        = "my_ecs_permissions"
   description = "Permissions to enable CT"
   policy = <<EOF
@@ -55,10 +55,10 @@ resource "aws_iam_policy" "ecs_permissions" {
   ]
 }
 EOF
-}
+}*/
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
- // policy_arn = "aws_iam_policy.ecs_permissions.arn"
+ //policy_arn = "aws_iam_policy.ecs_permissions.arn"
 }
