a1a22b518caa0bab9caaf51a35f7b26d089e6b59
Divya-Gunda <M1074001@granicus.com>
2022-09-26 09:31:50 +0530

    uploading ECR repo code42

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name               = var.ecs_task_execution_role
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
-/*
+
 # ECS task execution role policy attachment
 resource "aws_iam_policy" "ecs_permissions" {
   name        = "my_ecs_permissions"
@@ -55,10 +55,10 @@ resource "aws_iam_policy" "ecs_permissions" {
   ]
 }
 EOF
-}*/
+}
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  //policy_arn = "aws_iam_policy.ecs_permissions.arn"
+ // policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "aws_iam_policy.ecs_permissions.arn"
 }
