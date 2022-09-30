06f0e11f650d2e75027809daf703b7f5b07589a0
DivyaGunda16 <M1074001@granicus.com>
2022-09-30 17:31:45 +0530

    updated ecs service config37

--- launch_template.tf
+++ launch_template.tf
@@ -81,7 +81,7 @@ resource "aws_launch_template" "launch_template" {
 
   network_interfaces {
     associate_public_ip_address = true
-    //security_groups = [ aws_security_group.alb-sg.id ]
+    security_groups = [ aws_security_group.alb-sg.id ]
     //subnet_id = var.aws_subnet_private
   }
 
