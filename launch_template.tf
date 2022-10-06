b5f5ff9b06f1b8d66e63f0ff94d1b9844751bd7e
DivyaGunda16 <M1074001@granicus.com>
2022-10-06 12:03:15 +0530

    updated ecs service config

--- launch_template.tf
+++ launch_template.tf
@@ -80,7 +80,7 @@ resource "aws_launch_template" "launch_template" {
   }
 
   network_interfaces {
-    associate_public_ip_address = false
+    //associate_public_ip_address = false
     security_groups = [ "${aws_security_group.alb-sg.id}" ]
     subnet_id = var.aws_subnet_private1
   }
