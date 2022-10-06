01ec193850e8a4949ba6722bd16b69f75e42b16c
DivyaGunda16 <M1074001@granicus.com>
2022-10-06 13:28:34 +0530

    updated ecs service config

--- iam.tf
+++ iam.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "ecs-assume-role-policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["ecs.amazonaws.com"]
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
